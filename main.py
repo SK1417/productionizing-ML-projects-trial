6ecc916c28c5fad0c015a74b06f9286fa63bc83b
SK1417 <saikrishnaaug21@gmail.com>
2025-10-18 12:10:32 +0000

    first version of model pipeline

---
+++ main.py
@@ -0,0 +1,44 @@
+import pandas as pd 
+import numpy as np 
+from sklearn.preprocessing import OrdinalEncoder, StandardScaler, OneHotEncoder, LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
+
+df = pd.read_csv('churn.csv')
+num_cols = ['TotalCharges', 'MonthlyCharges', 'tenure']
+df[num_cols] = df[num_cols].apply(pd.to_numeric, errors='coerce')
+df['TotalCharges'] = df['TotalCharges'].fillna(df['MonthlyCharges'])
+df = df.drop(columns=['customerID'], axis=1)
+
+cols_le = ['gender', 'Partner', 'Dependents', 'PhoneService', 'PaperlessBilling']
+cols_ohe = [col for col in df.columns if col not in cols_le + num_cols + ['Churn']]
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('labels', OrdinalEncoder(), cols_le),
+        ('one_hot_encoding', OneHotEncoder(sparse_output=False), cols_ohe),
+        ('scaling', StandardScaler(), num_cols)
+    ]
+)
+
+pipeline = Pipeline([
+    ('preprocessing', preprocessor),
+    ('model', RandomForestClassifier())
+])
+
+
+X = df.drop(columns=['Churn'])
+y = df['Churn'].values
+
+y = LabelEncoder().fit_transform(y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y)
+
+pipeline.fit(X_train, y_train)
+
+print("Train Accuracy:", pipeline.score(X_train, y_train))
+print("Test Accuracy:", pipeline.score(X_test, y_test))
